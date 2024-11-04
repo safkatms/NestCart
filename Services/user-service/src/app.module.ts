f2ba7e1a9edbc560dcb9b94dba8fa49f308fd4e6
safkatms <safkatmahmudsakib@gmail.com>
2024-11-04 14:29:01 +0600

    Address Module Added

--- Services/user-service/src/app.module.ts
+++ Services/user-service/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './typeorm.config';
 import { UsersModule } from './users/users.module';
+import { AddressModule } from './address/address.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig),UsersModule,AuthModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig),UsersModule,AuthModule, AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
